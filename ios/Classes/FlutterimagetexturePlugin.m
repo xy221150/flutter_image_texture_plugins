87bf63fb3fc50f446850d32e69e851486ac0ffc2
xy221150 <xy221150@gmail.com>
2020-12-11 11:44:43 +0800

    feat:ios外接纹理

--- ios/Classes/FlutterimagetexturePlugin.m
+++ ios/Classes/FlutterimagetexturePlugin.m
@@ -1,15 +1,51 @@
 #import "FlutterimagetexturePlugin.h"
-#if __has_include(<flutterimagetexture/flutterimagetexture-Swift.h>)
-#import <flutterimagetexture/flutterimagetexture-Swift.h>
-#else
-// Support project import fallback if the generated compatibility header
-// is not copied when this plugin is created as a library.
-// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "flutterimagetexture-Swift.h"
-#endif
+#import "DuiaflutterextexturePresenter.h"
+
+NSObject<FlutterTextureRegistry> *textures;
+NSMutableDictionary<NSNumber *, DuiaflutterextexturePresenter *> *renders;
 
 @implementation FlutterimagetexturePlugin
+
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFlutterimagetexturePlugin registerWithRegistrar:registrar];
+    FlutterimagetexturePlugin *plugin = [[FlutterimagetexturePlugin alloc] init];
+    FlutterMethodChannel *channel = [FlutterMethodChannel methodChannelWithName:@"FlutterImageTexture" binaryMessenger:registrar.messenger];
+    [registrar addMethodCallDelegate:plugin channel:channel];
+    renders = [[NSMutableDictionary alloc] init];
+    textures = registrar.textures;
+}
+
+- (void)handleMethodCall:(FlutterMethodCall *)call result:(FlutterResult)result{
+    if([call.method  isEqual: @"getPlatformVersion"]){
+        NSString *version = [NSString stringWithFormat: @"%@%@", @"ios", UIDevice.currentDevice.systemVersion];
+        result(version);
+    }else if([call.method isEqualToString:@"load"]){
+        NSString *imageStr = call.arguments[@"url"];
+        Boolean asGif = [call.arguments[@"asGif"] boolValue];
+        CGFloat width = [call.arguments[@"width"] floatValue]*[UIScreen mainScreen].scale;
+        CGFloat height = [call.arguments[@"height"] floatValue]*[UIScreen mainScreen].scale;
+
+        CGSize size = CGSizeMake(width, height);
+        
+        DuiaflutterextexturePresenter *render = [[DuiaflutterextexturePresenter alloc] initWithImageStr:imageStr size:size asGif:asGif];
+        int64_t textureId = [textures registerTexture:render];
+
+        render.updateBlock = ^{
+            [textures textureFrameAvailable:textureId];
+        };
+        [renders setObject:render forKey:[NSString stringWithFormat:@"%@",@(textureId)]];
+       
+        result(@(textureId));
+    }else if([call.method isEqualToString:@"release"]){
+        if (call.arguments[@"id"]!=nil && ![call.arguments[@"id"] isKindOfClass:[NSNull class]]) {
+            DuiaflutterextexturePresenter *render = [renders objectForKey:call.arguments[@"id"]];
+            [renders removeObjectForKey:call.arguments[@"id"]];
+            [render dispose];
+            NSString *textureId =  call.arguments[@"id"];
+        
+            [textures unregisterTexture:@([call.arguments[@"id"] integerValue]).longValue];
+        }
+    }else {
+        result(FlutterMethodNotImplemented);
+      }
 }
 @end
